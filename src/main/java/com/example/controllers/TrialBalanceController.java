f3872b5216eb0a55b50b8427757c07ced4f0e7e6
AbuAmeera <abuameerax@gmail.com>
2022-02-10 04:09:26 +0200

    pull

--- src/main/java/com/example/controllers/TrialBalanceController.java
+++ src/main/java/com/example/controllers/TrialBalanceController.java
@@ -93,7 +93,7 @@ public class TrialBalanceController implements Initializable {
 
                     }
         }
-            
+
             listTrialBalanceTableView.add(trialBalanceTableView);
 
         }
